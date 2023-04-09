413b172d15483d9e5289299c326d615cff9d6c28
Felix Waller <felixwaller.public@gmail.com>
2023-04-09 23:43:52 +0100

    Update config
    - add astro types support
    - add type check before build
    - add import aliases
    - add content collections config

---
+++ src/content/config.ts
@@ -0,0 +1,26 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    excerpt: z.string(),
+    published: z.date(),
+    updated: z.date().optional(),
+    contents: z.boolean().default(true),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+const projectCollection = defineCollection({
+  schema: z.object({
+    name: z.string(),
+    href: z.string().url(),
+    tagline: z.string(),
+    featured: z.boolean().optional(),
+  }),
+});
+
+export const collections = {
+  "blog": blogCollection,
+  "projects": projectCollection,
+};
