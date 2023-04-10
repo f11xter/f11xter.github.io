0b0cff011b7220d47d04fbba868dbf123a94ece2
Felix Waller <felixwaller.public@gmail.com>
2023-04-10 11:09:15 +0100

    Add GitHub deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://f11xter.github.io",
+});
