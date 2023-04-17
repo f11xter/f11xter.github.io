e39496a47583d54b1221cedebf5d0c9b17500701
Felix Waller <felixwaller.public@gmail.com>
2023-04-17 20:10:53 +0100

    Add rss support
    - add packages
      - @astrojs/rss
      - markdown-it
      - sanitize-html
    - add `src/feed.xml.js`
    - add feed anchor to home, blog list and blogs
    - add `public/rss.xsl` for feed browser styling

---
+++ src/pages/feed.xml.js
@@ -0,0 +1,24 @@
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
+
+export async function get(context) {
+  const blog = await getCollection("blog");
+
+  return rss({
+    title: "Felix Waller",
+    description: "programmer & musician",
+    site: context.site,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.published,
+      description: post.data.excerpt,
+      content: sanitizeHtml(parser.render(post.body)),
+      link: `/blog/${post.slug}/`,
+    })),
+    stylesheet: "/rss.xsl",
+  });
+}
